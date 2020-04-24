dfd0b8eeeaad20ceaaba17e1a3392fdae865e475
Martin Butler <mratin@gmail.com>
2020-04-24 08:02:49 +0200

    Updates

---
+++ precache-manifest.929c50fdf1423618bfed9969fd9a6876.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60486d514641854c4ba1299454cd398d",
+    "url": "/corona-js/index.html"
+  },
+  {
+    "revision": "65c134345f48d72f3397",
+    "url": "/corona-js/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "41f8c128cf77ce02a00c",
+    "url": "/corona-js/static/js/2.01b8649b.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/corona-js/static/js/2.01b8649b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65c134345f48d72f3397",
+    "url": "/corona-js/static/js/main.40b2d901.chunk.js"
+  },
+  {
+    "revision": "2df80a37708d6c425f42",
+    "url": "/corona-js/static/js/runtime-main.5c107bc7.js"
+  }
+]);
